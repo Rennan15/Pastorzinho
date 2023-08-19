93299b9b1dbe7b53b328a648355a8db744d7d6b0
Rennan15 <142065290+Rennan15@users.noreply.github.com>
2023-08-19 12:22:49 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Beep(1320, 200); Console.Beep(1485, 200); Console.Beep(1650, 200); Console.Beep(1759, 200); Console.Beep(1759, 200); Console.Beep(1759, 200);
+Thread.Sleep(1000);
+Console.Beep(1320, 200); Console.Beep(1485, 200); Console.Beep(1320, 200); Console.Beep(1485, 200); Console.Beep(1485, 200); Console.Beep(1485, 200);
+Thread.Sleep(1000);
+Console.Beep(1650, 200); Console.Beep(1980, 200); Console.Beep(1759, 200); Console.Beep(1650, 200); Console.Beep(1650, 200); Console.Beep(1650, 200);
+Thread.Sleep(1000);
+Console.Beep(1320, 200); Console.Beep(1485, 200); Console.Beep(1650, 200); Console.Beep(1759, 200); Console.Beep(1759, 200); Console.Beep(1759, 200);
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Pastorzinho
+# Exercicio_Pastorzinho
 Faça um programa que emita beeps no tom da canção citada em "O Pastorzinho".
